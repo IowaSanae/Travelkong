3dda0d7043ddde5c5944e86bbdfe63dffcc9bbe7
Butter Fish <sun1231010@gmail.com>
2021-05-23 18:29:37 +0700

    update

--- main.js
+++ main.js
@@ -2,4 +2,15 @@
 /*loadDoc = () => {
     return "Hello World.";
 }*/
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
 loadDoc = () => "Hello World.";
