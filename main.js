d193fca12c52c8c1e2d135560a06151ed2da3e98
Butter Fish <sun1231010@gmail.com>
2021-07-30 15:58:32 +0700

    update even more stuff

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-//loadDoc = function loadDoc() {}
-/*loadDoc = () => {
-    return "Hello World.";
-}*/
-
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-
-loadDoc = () => "Hello World.";
+$('carousel-inner').carousel ({
+    interval: 5000
+});
