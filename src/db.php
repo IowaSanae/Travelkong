08ac5ce62562caac717985160a1747c14ee210c0
Đỗ Gia Bảo <62286955+IowaSanae@users.noreply.github.com>
2021-11-14 12:02:25 +0700

    Changing the language back to Nodejs

--- src/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = mysqli_connect("localhost", "root", "", "travelkong_db");
-
-if ($mysqli -> connect_errno) {
-  echo ("Không thể kết nối đến SQL vì lỗi: " . $mysqli -> connect_error);
-  exit();
-}
-?>
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+const mongoose = require('mongoose')
+const express = require('express')
+
+// For carousel.
 $('carousel-inner').carousel({
     interval: 5000
 });
+
